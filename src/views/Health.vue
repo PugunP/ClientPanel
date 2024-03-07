d54c7eeeac4533feefefa243f300f51fecef99a7
Popwichh <161027884+Popwichh@users.noreply.github.com>
2024-03-07 23:41:30 +0700

    test

--- src/views/Health.vue
+++ src/views/Health.vue
@@ -1,6 +1,33 @@
 <template>
 	<main id="TableList_Page">
-		<h1 class="text-red-500">Health</h1>
-		<p>This is the Health page</p>
+		<h1 class=" px-10 text-red-500 text-3xl h-12 my-10">ตรวจสุขภาพ</h1>
+		<div class="flex flex-col px-10 my-10">
+			<div
+			class="flex flex-col-2 "
+			>
+			<div
+			class="mb-10"
+			>
+
+				<img src="../assets//bloodPressureMeter.jpg" class=" h-60 w-120" alt="Your Image">
+			</div>
+			<div>
+				<p>ตัวดันตัวบน : 120 </p>
+				<p>ความดันตัวล่าง : 70 </p>
+				<p>ชีพจร : 70 </p>
+			</div>
+			</div>
+			<div
+			class="flex flex-col-2"
+			>
+			<div>
+
+				<img src="../assets/oxiMeter.jpg" class=" h-60 w-120" alt="Your Image">
+			</div>
+			<div>
+				<p>ปริมาณออกซิเจนในเลือด : 98</p>
+			</div>
+			</div>
+</div>
 	</main>
 </template>
