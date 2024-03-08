3f7c6805db74e4f0fdbcdbd2fbf182f695493315
Popwichh <161027884+Popwichh@users.noreply.github.com>
2024-03-08 21:34:02 +0700

    เพิ่มปุ่มลิงค์ไปหน้าแสดงข้อมูล

--- src/views/Health.vue
+++ src/views/Health.vue
@@ -1,7 +1,19 @@
 <template>
 	<main id="TableList_Page">
 	  <h1 class="px-5 font-black text-3xl h-12 my-10">ตรวจสุขภาพ</h1>
-	  <div class="flex flex-col px-10 my-10">
+	  <div 
+	  	v-if="!hasHealthData"
+	 	class="flex flex-col items-center">
+      <h1 class="text-2xl text-center my-5">ไม่เคยมีประวัติการตรวจ</h1>
+      <button 
+	  	@click="showHealthData"
+	 	class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        เพิ่มประวัติการตรวจ
+      </button>
+    </div>
+	  <div 
+	  	v-if="hasHealthData"
+	 	class="flex flex-col px-10 my-10">
 		<div class="pb-12 px-6 flex flex-col-2 items-center">
 		  <div class="w-64 h-64 overflow-hidden flex items-center justify-center border border-black">
 			<img src="../assets/bloodPressureMeter.jpg" class="w-full h-full object-cover" alt="Your Image">
@@ -23,7 +35,31 @@
 			<p>4.ปริมาณออกซิเจนในเลือด (%SpO2)</p>
 		  </div>
 		</div>
+		<button @click="showHealthData" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-8">
+        เพิ่มประวัติการตรวจ
+      </button>
 	  </div>
 	</main>
   </template>
   
+  <script>
+    import { ref } from 'vue';
+
+    export default {
+      setup() {
+        // ใช้ ref เพื่อสร้าง reactive data
+        const hasHealthData = ref(false);
+
+        // method ที่ให้เรียกเมื่อปุ่มถูกคลิก
+        const showHealthData = () => {
+          // เปลี่ยนค่าเพื่อแสดงประวัติการตรวจ
+          hasHealthData.value = !hasHealthData.value;
+        };
+
+        return {
+          hasHealthData,
+          showHealthData,
+        };
+      },
+    };
+  </script>
