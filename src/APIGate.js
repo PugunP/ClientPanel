c166bc28c2df5c6ab44a8d3cfde0816192b46e7e
PAKIN JINJAI <pakin.281244@gmail.com>
2024-04-03 23:56:51 +0700

    Update latest API endpoint

--- src/APIGate.js
+++ src/APIGate.js
@@ -5,12 +5,26 @@
 const baseURL = "http://localhost:8080/api" 
 const LOGIN_API = "/user-login";
 const REGISTER_API = "/user-register";
-const UPLOADIMAGE_API = "/upload-images-me"; 
+
+/** 'Authorization': `Bearer Include accessToken in the header'Content-Type': 'application/json' **/ 
+const UPLOADIMAGE_API = "/upload-images-me";
+
+/** 'Authorization': `Bearer Include accessToken in the header'Content-Type': 'application/json' **/ 
 const GETIMAGE_API = "/get-images"; //อย่าลืม / {userId}
+
+/** 'Authorization': `Bearer Include accessToken in the header'Content-Type': 'application/json' **/ 
+const GETALLIMAGE_MEAPI = '/get-all-images-me'
+
+/** 'Authorization': `Bearer Include accessToken in the header'Content-Type': 'application/json' **/ 
+const UPDATEUSER = '/user-update'
+
+
 export {
     baseURL,
     LOGIN_API,
     REGISTER_API,
     UPLOADIMAGE_API,
+    GETALLIMAGE_MEAPI,
+    UPDATEUSER,
     GETIMAGE_API
 }
